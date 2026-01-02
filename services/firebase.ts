6909c633d87d913f9479b8b63caa066ae8c11701
Nguyễn Công Thành <nctxxxxxx@gmail.com>
2026-01-02 23:13:27 +0700

    Secure Firebase credentials

--- services/firebase.ts
+++ services/firebase.ts
@@ -7,13 +7,13 @@ import { getFirestore } from 'firebase/firestore';
 // 2. Copy thông tin trong firebaseConfig và thay thế vào bên dưới:
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC-JZrio92RycE7Hios7TvUr_xRcUG9s0A",
-  authDomain: "portfolio-cms-18f16.firebaseapp.com",
-  projectId: "portfolio-cms-18f16",
-  storageBucket: "portfolio-cms-18f16.firebasestorage.app",
-  messagingSenderId: "372191394576",
-  appId: "1:372191394576:web:7bd70cbd2bbd86743c4395",
-  measurementId: "G-LNHCVK4PFR"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase (Modular SDK)
